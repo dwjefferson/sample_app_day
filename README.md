76b1f892fa6d7becdc9b7032caa63ff6cdd1b0f7
Dean Jefferson <djefferson@madisoncollege.edu>
2013-04-08 14:03:27 -0500

    Finish user edit, update, index, and destroy actions

--- README.md
+++ README.md
@@ -4,6 +4,4 @@ This is the sample application for
 [*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.org/)
 by [Michael Hartl](http://michaelhartl.com/).
 
-4/11/2013 - finished Ch. 8.2.1; start Ch. 8.2.2 Wed.
-
-Goal for 4/13 is to finish Ch. 8.
+4/3/2013 - finished Ch. 9 section 9.3.2; start section 9.3.3.
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     userx = User.find_by_email(params[:session][:email].downcase)
     if userx && userx.authenticate(params[:session][:password])
         sign_in userx
-      redirect_to userx
+      redirect_back_or userx
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
